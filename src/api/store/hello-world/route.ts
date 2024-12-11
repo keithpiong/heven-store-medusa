f69489e6817a48394cc12ee281723220024775ad
keithpiong <keith.piong@gmail.com>
2024-12-12 00:04:30 +0800

    added hello world testing

---
+++ src/api/store/hello-world/route.ts
@@ -0,0 +1,7 @@
+import type { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
+
+export async function GET(req: MedusaRequest, res: MedusaResponse) {
+  res.json({
+    message: "Hello world!",
+  });
+}
